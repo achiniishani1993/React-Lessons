23ce31e599404d1b1576d8b8fb00ceb7caeb9028
achiniishani1993 <achiniishani1993@gmail.com>
2026-04-21 20:35:28 +0200

    add other two project details

--- src/components/project/Projects.jsx
+++ src/components/project/Projects.jsx
@@ -11,12 +11,12 @@ function Projects({
 }) {
   return (
     <div className="flex justify-center">
-      <div className="flex gap-6 bg-black p-6 rounded-xl shadow-[0_0_25px_rgba(255,255,255,0.5)]">
+      <div className="flex gap-6 bg-[#2b4a46] text-white p-6 rounded-xl shadow-[0_0_25px_rgba(0,0,0,0.5)]">
         <div className="w-50" >
           <h3 className="text-center mb-2">{name}</h3>
           <img src={screenshot} alt="project UI" className="w-full  object-cover rounded-md" />
         </div>
-        <div className="w-50 space-y-2 ">
+        <div className="w-50 space-y-2  mt-4">
           <p>
             <span className="font-semibold">Tech Used :</span> {Tech}
           </p>
