4b51de43796b78c11f2816d83b9cc85cdaeec693
achiniishani1993 <achiniishani1993@gmail.com>
2026-04-21 12:09:22 +0200

    installed tailwind

---
+++ src/components/project/Projects.jsx
@@ -0,0 +1,14 @@
+
+const Projects = ({name, screenshot, Github, Tech, background, purpose, skills, role, challenges}) => {
+  return (
+    <div>
+        <h1>My Pervious Projects</h1>
+        <P>I have selected three projects that I have completed so far. These projects showcase my skills and demonstrate 
+            how I have continued to learn and improve over time.</P>
+
+        
+    </div>
+  )
+}
+
+export default Projects
