51f022d42088bf42fa75efda9fe8f5d6e0d9e3f3
achiniishani1993 <achiniishani1993@gmail.com>
2026-04-24 10:53:00 +0200

    imported BrowserRouter, Routes and Route  app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter as Router, Routes,Route, BrowserRouter } from 'react-router-dom';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
 
 
 const App = () => {
   return (
-    <div >
-  
-    
-    </div>
+    <Router>
+      <div>
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Routes>
+      </div>
+    </Router>
+
   );
 };
 
